17a0c8b9efb2b3227509ab58132ad320514e75dc
yguan <pipehappy@pipehappy.me>
2016-08-04 21:32:49 +0800

    update script to use new format

--- subtitle.py
+++ subtitle.py
@@ -1,62 +1,70 @@
 import string
 import re
 from math import floor
-import _winreg
+
 import sys
 
-def get_desktop():
-	key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders',)
-	return _winreg.QueryValueEx(key, "Desktop")[0]
-
-assTime = re.compile('^\d+:\d+:\d+\.\d+')
+assTime = re.compile('^\d+:\d+:\d+\,\d+')
 
 def assStringToTime(strr):
-	t = strr.split(':')
-	time = 0
-	time += int(t[0]) * 3600
-	time += int(t[1]) * 60
-	time += float(t[2])
-	return time
+    (strr0, f0) = strr.split(',')
+    t = strr0.split(':')
+    time = 0
+    time += int(t[0]) * 3600
+    time += int(t[1]) * 60
+    time += float(t[2])
+    time += float('0.' + f0)
+    return time
 
 def assTimeToString(time):
-	sec = floor(time)
-	part = str(int((time - sec) * 100))
-	if len(part) < 2:
-		part = '0' + part
-	h = str(int(sec // 3600))
-	sec %= 3600
-	m = str(int(sec // 60))
-	if len(m) < 2:
-		m = '0' + m
-	sec = str(int(sec%60))
-	if len(sec) < 2:
-		sec = '0' + sec
-	strr = h +':'+ m +':'+ sec + '.' + part
-	return strr
+    sec = floor(time)
+    part = str(int((time - sec) * 100))
+    if len(part) < 2:
+        part = '0' + part
+    h = str(int(sec // 3600))
+    sec %= 3600
+    m = str(int(sec // 60))
+    if len(m) < 2:
+        m = '0' + m
+    sec = str(int(sec%60))
+    if len(sec) < 2:
+        sec = '0' + sec
+    strr = h +':'+ m +':'+ sec + ',' + part
+    return strr
 
 def processAssLine(line,offset):
-	li = line.split(',')
-	if len(li) >= 3:
-		if assTime.match(li[1]):
-			tt1 = assStringToTime(li[1]) + offset
-			li[1] = assTimeToString(tt1)
-		if assTime.match(li[2]):
-			tt2 = assStringToTime(li[2]) + offset
-			li[2] = assTimeToString(tt2)
-	return ','.join(li)
+    if line.find('-->') > 0:
+        line = line.strip()
+        li = line.split(' --> ')
+        if len(li) == 2:
+            if assTime.match(li[0]):
+                tt1 = assStringToTime(li[0]) + offset
+                li[0] = assTimeToString(tt1)
+            if assTime.match(li[1]):
+                tt2 = assStringToTime(li[1]) + offset
+                li[1] = assTimeToString(tt2)
+        return ' --> '.join(li) + '\n'
+    else:
+        return line
+
+
 
 assert len(sys.argv) >= 3, "Lack of Arguments"
 
 inputfile = sys.argv[1]
-filenames = inputfile.split("\\")
+filenames = inputfile.split("/")
 filename = filenames[len(filenames) - 1]
 offset = int(sys.argv[2])
 
+print(inputfile)
+print("./new." + filename)
+print(offset)
+
 f = open(inputfile, 'r')
-t = open(get_desktop() + "\\new." + filename ,'w')
+t = open("./new." + filename ,'w')
 for line in f.readlines():
-	ans = processAssLine(line,offset)
-	t.write(ans)
+    ans = processAssLine(line,offset)
+    t.write(ans)
 
 f.close()
 t.close()
